b6d48e075b3c7c2d0f433af49c58240f27c56293
chmolto <chfus@proton.me>
2026-01-27 09:32:53 +0100

    fix: remove files

--- read_build.py
+++
@@ -1,8 +0,0 @@
-
-with open('build_output.txt', 'rb') as f:
-    content = f.read()
-    try:
-        text = content.decode('utf-16')
-        print(text)
-    except:
-        print(content.decode('utf-8', errors='ignore'))
